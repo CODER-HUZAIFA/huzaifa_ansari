22c4784e8c1cb2ba22d14a9ed6957a3e7a58f90c
CODER-HUZAIFA <ansarihuzaifa9825@gmail.com>
2025-01-09 19:03:40 +0530

    About Page Completed

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,10 +1,43 @@
 import React from 'react'
+import Profile from '../Hero/Intro/Profile'
+import './About.css'
 
 const About = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="w-full mt-[84px] flex justify-around">
+        <div className="">
+          <div className="mb-[20px]">
+            <p className='aboutMyName'>Meet - <span>HUZAIFA ANSARI</span></p>
+            <p className='aboutMySelf'>Hi! I’m Huzaifa Ansari, a 16-year-old software engineer with a passion for creating, innovating, and pushing the boundaries of technology. 💻</p>
+            <p className='aboutMySelf'>I specialize as a Full-Stack Developer, mastering both Backend 🌐 and Frontend 🎨 development. With expertise in Node.js, JavaScript ✨, Python 🐍, Java ☕, and Flutter 📱, I bring ideas to life through code.</p>
+          </div>
+          <div className="mb-[20px]">
+            <p className='aboutMySelfHeading'>🎮 My Skills</p>
+            <p className='aboutMySelf'>Beyond apps and websites, I also dive into the exciting world of game development using Unity Engine 🕹️, creating interactive and fun gaming experiences.</p>
+          </div>
+          <div className="mb-[20px]">
+            <p className='aboutMySelfHeading'>🌱 Always Learning</p>
+            <p className='aboutMySelf'>My curiosity drives me to constantly learn and grow. I’m exploring the fascinating realm of Artificial Intelligence 🤖, understanding how to build AI models that can shape the future of technology.</p>
+          </div>
+          <div className="mb-[20px]">
+            <p className='aboutMySelfHeading'>🌍 My Vision</p>
+            <p className='aboutMySelf'>My dream of building an IT startup at a young age—a platform where people can easily find jobs and unlock their true potential. By blending creativity, technology, and purpose, I aim to make a meaningful impact on the world.</p>
+            <br />
+            <p className='aboutMySelf'>Let’s collaborate, innovate, and build something extraordinary together! 🚀</p>
+          </div>
+        </div>
+        <div className="flex flex-col items-center gap-[20px] h-fit">
+          <div className=""><Profile /></div>
+          <div className="quote flex flex-col items-center mt-[50px]">
+            <p className='quoteTxt'>Fueled by passion, driven by dreams,</p>
+            <p className='quoteTxt'>I build a future with code that gleams,</p>
+            <p className='quoteTxt'>Where tech empowers and brightly streams,</p>
+            <p className='quoteTxt'>Turning ideas into reality’s beams.</p>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import './Footer.css'
+import { NavLink } from 'react-router-dom'
+
 const Footer = () => {
   return (
     <>
-        <div className="mt-[30px] w-full flex items-center justify-center flex-col gap-[10px]">
+        <div className="mt-[50px] py-1 border-t-[1px] border-black w-full flex items-center justify-center flex-col gap-[10px]">
             <div className="profileFooterImg flex gap-[10px] items-center">
                 <img src="images/profile_image.svg" className='rounded-full h-[35px] w-[35px]' alt="" />
                 <p>HUZAIFA ANSARI</p>
             </div>
             <div className="listsFooter">
                 <ul className='flex gap-[15px]'>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Services</a></li>
-                    <li><a href="#">Contact</a></li>
+                    <li><NavLink to="/">Home</NavLink></li>
+                    <li><NavLink to="/about">About</NavLink></li>
+                    <li><NavLink to="/services">Services</NavLink></li>
+                    <li><NavLink to="/contact">Contact</NavLink></li>
                 </ul>
             </div>
         </div>
